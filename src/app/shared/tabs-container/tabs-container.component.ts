9adf86cbb0f4c80047644510626137bf10671728
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2023-01-23 12:10:11 +0200

    login/register tabs set to active or false

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ContentChildren, AfterContentInit, QueryList } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
 
 @Component({
   selector: 'app-tabs-container',
   templateUrl: './tabs-container.component.html',
   styleUrls: ['./tabs-container.component.css']
 })
-export class TabsContainerComponent implements OnInit {
+export class TabsContainerComponent implements AfterContentInit {
+
+  @ContentChildren(TabComponent) tabs: QueryList<TabComponent> = new QueryList();
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterContentInit(): void {
+    const activeTabs = this.tabs?.filter(
+      tab => tab.active
+    )
+
+    if(!activeTabs || activeTabs.length ===0){
+      this.selectTab(this.tabs!.first);
+    }
   }
+
+  selectTab(tab: TabComponent){
+    // first turn every tab off
+    this.tabs?.forEach(tab => {
+      tab.active = false
+    })
+
+    tab.active = true;
+  }
+
+  
 
 }
