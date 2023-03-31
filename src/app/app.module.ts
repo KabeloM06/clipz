fd49d3cc7c66b6bdc5b1ac4d69ed42cd49466606
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2023-03-31 18:54:28 +0200

    Registered routes and home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,14 @@ import { NavComponent } from './nav/nav.component';
 import { AngularFireModule } from '@angular/fire/compat';
 import {AngularFireAuthModule} from '@angular/fire/compat/auth';
 import { environment } from 'src/environments/environment';
-import {AngularFirestoreModule} from '@angular/fire/compat/firestore'
+import {AngularFirestoreModule} from '@angular/fire/compat/firestore';
+import { HomeComponent } from './home/home.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
